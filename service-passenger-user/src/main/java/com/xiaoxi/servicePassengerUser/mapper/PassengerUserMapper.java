52e19ec586c62f8d4e4483be3fdf76befdf7f63f
耿艺航 <3082477380@qq.com>
2023-12-18 20:34:46 +0800

    1.0.8service-passenger-user根据手机号，查询用户信息

---
+++ service-passenger-user/src/main/java/com/xiaoxi/servicePassengerUser/mapper/PassengerUserMapper.java
@@ -0,0 +1,9 @@
+package com.xiaoxi.servicePassengerUser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaoxi.servicePassengerUser.dto.PassengerUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
--- service-passenger-user/src/main/java/com/xiaoxi/servicePassengerUser/service/UserService.java
+++ service-passenger-user/src/main/java/com/xiaoxi/servicePassengerUser/service/UserService.java
@@ -1,14 +1,29 @@
 package com.xiaoxi.servicePassengerUser.service;
 
 import com.xiaoxi.interfaceCommon.dto.ResponseResult;
+import com.xiaoxi.servicePassengerUser.dto.PassengerUser;
+import com.xiaoxi.servicePassengerUser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
+
     public ResponseResult logOrReg(String passengerPhone) {
         System.out.println("查询用户信息");
         //根据手机号查询用户信息
+        Map<String, Object> map = new HashMap<>();
+        map.put("passenger_phone",passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size() == 0?"无记录":passengerUsers.get(0).getPassengerName());
 
         //判断用户是否存在
 
