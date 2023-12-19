ead1e32cb144809661113a1c71c55ee96ee1fe91
耿艺航 <3082477380@qq.com>
2023-12-19 21:39:38 +0800

    1.1.5测试token的controller方法

--- api-passenger/src/main/java/com/xiaoxi/apiPassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/xiaoxi/apiPassenger/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.xiaoxi.apiPassenger.controller;
 
+import com.xiaoxi.interfaceCommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "api passenger test";
+    }
+
+    @GetMapping("/authTest")
+    public ResponseResult authTest() {
+        return ResponseResult.success("auth test");
+    }
+
+    @GetMapping("/noAuthTest")
+    public ResponseResult noAuthTest() {
+        return ResponseResult.success("noAuth test");
     }
 }
