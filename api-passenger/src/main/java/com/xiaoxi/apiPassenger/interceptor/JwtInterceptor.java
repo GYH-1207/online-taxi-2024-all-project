e99dc2859c24481b49a56b65b923c65b43dd1f81
耿艺航 <3082477380@qq.com>
2023-12-19 21:55:16 +0800

    1.1.6token拦截器的编写

---
+++ api-passenger/src/main/java/com/xiaoxi/apiPassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,49 @@
+package com.xiaoxi.apiPassenger.interceptor;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.xiaoxi.interfaceCommon.dto.ResponseResult;
+import com.xiaoxi.interfaceCommon.util.JwtUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        boolean result = true;
+        String resultStr = "";
+
+        //获取token
+        String token = request.getHeader("Authorization");
+
+        try {
+            //解析token
+            JwtUtils.parseToken(token);
+        } catch (SignatureVerificationException e) {
+            resultStr = "token sign error";
+            result = false;
+        } catch (TokenExpiredException e) {
+            resultStr = "token time out";
+            result = false;
+        } catch (AlgorithmMismatchException e) {
+            resultStr = "token AlgorithmMismatchException";
+            result = false;
+        } catch (Exception e) {
+            resultStr = "token invalid";
+            result = false;
+        }
+
+        if(!result) {
+            PrintWriter printWriter = response.getWriter();
+            printWriter.write(JSONObject.toJSONString(ResponseResult.fail(resultStr).toString()));
+        }
+
+        return true;
+    }
+}
